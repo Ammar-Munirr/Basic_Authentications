455b5e52e063d7bc57e5b1819baad56c74cdcfb1
Ammar Munir <munirammar0@gmail.com>
2025-05-10 23:14:41 +0500

    setup urls

--- api/users/views.py
+++ api/users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def redirect_api(request):
+    return redirect('schema-swagger-ui')
--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from api.users.views import redirect_api
+
+
 urlpatterns = [
+    path("", redirect_api,name='redirect'),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
